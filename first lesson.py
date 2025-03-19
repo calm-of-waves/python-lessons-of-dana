0edd5b34cbed9d9494a1d4c8c998f876f51dd26d
Saparova Jennet <turkmenbashycity.2024@gmail.com>
2025-03-19 17:29:42 +0500

    Add files via upload

---
+++ first lesson.py
@@ -0,0 +1,19 @@
+# a = 'Hello'
+# b = 'World'
+# print(a,b, sep=' & ') #this is a comment
+
+# name = input('Write your name: ')
+# age = input('Write ur age: ')
+# name = 'Your name is ' + name
+# age = 'You are ' + age + " years old"
+# print(name)
+# print(age)
+
+# c = 12
+# d = 2
+# f = (c + d)*d
+# print('joagby: ' + str(f))
+
+h = input('your age: ')
+j = 100 + int(h)
+print('understandable:' + str(j))
