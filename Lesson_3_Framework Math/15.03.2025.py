6cd7204924518988760cb125b4fa58306724ca3d
Saparova Jennet <turkmenbashycity.2024@gmail.com>
2025-05-16 00:02:13 -0700

    Add files via upload

---
+++ Lesson_3_Framework Math/15.03.2025.py
@@ -0,0 +1,70 @@
+import math
+        # Mesele N1
+# kg = int(input('Suyjunin umumy kg sany: '))
+# baha = int(input('harydyn umumy bahasy: '))
+# taze_kg = int(input('alan harydynyzyn kg sany: '))
+# y = baha / kg
+# kg2 =  y * taze_kg
+# print(kg2)
+
+            # Mesele N2
+# Chocolate_kg = int(input('Shekeladyn umumy kg-y nace boly: '))
+# price_of_chocolate = int(input('Shekeladyn umumy bahasy: '))
+# Iriska_kg = int(input('iriskanyn umumy kg sany: '))
+# price_of_Iriska = int(input('Alan iriskanynyz umumy bahasy: '))
+
+# Chocolate_1kg = price_of_chocolate / Chocolate_kg
+
+# Iriska_1kg = price_of_Iriska / Iriska_kg
+
+# Sony = Chocolate_1kg / Iriska_1kg
+# print(Sony)
+
+            # Mesele N3😕
+# L = int(input('uzunlygy: '))
+# doly_metr = L // 100     # L yazylan san 100-bir metrda 100 sn bar bolany ucin 
+# print(L, 'sm-de', doly_metr, 'metr bar')
+
+
+            # Mesele  N4
+# baytyn_sany = int(input('baytyn sany: '))
+# kilobayt = baytyn_sany // 1024
+# print(baytyn_sany, 'bayt', kilobayt, 'kilobayt bolya')
+
+                # Mesele N 4    (2 belgili san yazanda, sholary aşak aşagyna yazdyzmak uçin app)
+# dc = int(input('2 belgili san yaz: '))
+# S = dc // 10
+# j = dc % 10
+# print(S)
+# print(j)
+
+            # Mesele N5
+rm = int(input('yuzluk san yaz: '))
+onluk = rm // 10 % 10
+birlik = rm % 10
+print(onluk)
+print(birlik)
+
+#             Mesele N6
+# num =  int(input('San yaz: '))
+# onluk = num // 10 % 10 
+# birlik = num % 10
+# yuzluk = num // 100
+# print(onluk,yuzluk,birlik)
+
+                # Mesele N7
+# N = int(input('Taze sutkadan nace sekunt gecdi: '))
+# dolu_minut = N // 60
+# print(dolu_minut)
+
+
+                # Mesele N 8
+K = int(input('Nacinji yyl: '))
+M = K / 100 
+M = math.ceil(M)
+print(M)
+
+
+
+
+
